c7bcad99a86b42b5aa09c10c1a58588c7db829fc
Christopher Vachon <cvachon@prometrix.ca>
2023-11-27 12:13:43 -0500

    Update for PNPM and Git Version Merge Testing

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,10 @@ git clone git@github.com:CodeVachon/CodeVachon.git test-repo
 cd test-repo
 git checkout -b test
 git checkout main
+git fetch
+git checkout Patch-v1.0.1 || exit 1
+git pull
+git checkout Release-v1.1.0 || exit 1
+git pull
 git checkout -b banana
 git checkout -b production
